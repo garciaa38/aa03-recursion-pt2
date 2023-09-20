5035a99e361018ab0c53b411b0b71b5a9a400c3c
Christian E <christianelena92@gmail.com>
2023-09-20 17:36:36 -0500

    solve add-to-twelve

--- problems/06-add-to-twelve.js
+++ problems/06-add-to-twelve.js
@@ -12,11 +12,23 @@ addToTwelve([1, 12, 4, 7, 6]); // false
 addToTwelve([1]); // false
 ***********************************************************************/
 
-
+// input: array of numbers
+// return boolean 
 function addToTwelve(arr) {
-  // Your code here 
-}
+  // base case 
+  if (arr.length <= 1) return false; 
 
+  // first elem in array + second elem in the array
+  if (arr[0] + arr[1] === 12) return true; 
+  // recusive case
+  
+  return addToTwelve(arr.slice(1)); 
+}
+// console.log(addToTwelve([1, 3, 4, 7, 5])); // true
+// console.log(addToTwelve([1, 3, 4, 7, 6])); // false
+// console.log(addToTwelve([1, 11, 4, 7, 6])); // true
+// console.log(addToTwelve([1, 12, 4, 7, 6])); // false
+// console.log(addToTwelve([1])); // false
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 try {
