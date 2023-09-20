7738f23adb016322bf230d1a231d51617c5f1399
Andrew Garcia <garciaa38@gmail.com>
2023-09-20 18:28:59 -0400

    problem 5 solved

--- problems/05-reverse.js
+++ problems/05-reverse.js
@@ -14,10 +14,27 @@ reverse(""); // ""
 
 
 function reverse(str) {
-  // Your code here 
+  // Your code here
+  // Base case: When there is nothing in the string left.
+  if (str.length === 0) {
+    return '';
+  }
+
+  let newStr = str.slice(str.length - 1);
+
+  return newStr + reverse(str.slice(0, str.length - 1));
+  //newStr += str.slice(str.length - 1);
+  //console.log(newStr);
+
+
 }
 
-
+console.log(reverse("house")); // "esuoh"
+console.log(reverse("dog")); // "god"
+console.log(reverse("atom")); // "mota"
+console.log(reverse("q")); // "q"
+console.log(reverse("id")); // "di"
+console.log(reverse("")); // ""
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 try {
   module.exports = reverse;
